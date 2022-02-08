82fb066bcfbf78f440ddc20563a1209793fe4d02
Darshan Nagesh <darshan.nagesh@tarento.com>
2022-02-08 15:23:15 +0530

    Changes to gateway for unauthorized requests

--- src/main/java/org/tarento/retail/filters/pre/RbacFilter.java
+++ src/main/java/org/tarento/retail/filters/pre/RbacFilter.java
@@ -1,9 +1,11 @@
 package org.tarento.retail.filters.pre;
 
-import com.netflix.zuul.ZuulFilter;
-import com.netflix.zuul.context.RequestContext;
+import static org.tarento.retail.constants.RequestContextConstants.ERROR_CODE_KEY;
+import static org.tarento.retail.constants.RequestContextConstants.ERROR_MESSAGE_KEY;
+import static org.tarento.retail.constants.RequestContextConstants.RBAC_BOOLEAN_FLAG_NAME;
+import static org.tarento.retail.constants.RequestContextConstants.USER_INFO_KEY;
 
-import static org.tarento.retail.constants.RequestContextConstants.*;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +13,10 @@ import org.springframework.http.HttpStatus;
 import org.tarento.retail.contract.Action;
 import org.tarento.retail.contract.User;
 import org.tarento.retail.exceptions.zuulExceptions.RbacException;
+import org.tarento.retail.util.ExceptionUtils;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 
 /**
  *  5th pre filter to get executed.
@@ -37,29 +43,29 @@ public class RbacFilter extends ZuulFilter{
     }
 
     @Override
-    public Object run() {
-        RequestContext ctx = RequestContext.getCurrentContext();
-        try { 
-        	final boolean isIncomingURIInAuthorizedActionList = isIncomingURIInAuthorizedActionList(ctx);
-        	if(isIncomingURIInAuthorizedActionList) {
-        		ctx.set("RbacAvailable",Boolean.TRUE);
-        		return null;
-        	}
-        	else { 
-        		ctx.set("RbacAvailable",Boolean.FALSE);
-        	}
-        } catch (Exception e) { 
-        	logger.error("Failed : " +e) ;
-        	abortWithStatus(ctx,HttpStatus.FORBIDDEN, FORBIDDEN_MESSAGE);
-            throw e;
-        }
-        return null;
-    }
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+
+		final boolean isIncomingURIInAuthorizedActionList = isIncomingURIInAuthorizedActionList(ctx);
+		if (isIncomingURIInAuthorizedActionList) {
+			ctx.set("RbacAvailable", Boolean.TRUE);
+			return null;
+		} else {
+			ctx.set("RbacAvailable", Boolean.FALSE);
+			ExceptionUtils.raiseCustomException(HttpStatus.FORBIDDEN, FORBIDDEN_MESSAGE);
+		}
+
+		return null;
+	}
 
     private boolean isIncomingURIInAuthorizedActionList(RequestContext ctx) {
         String requestUri = ctx.getRequest().getRequestURI();
         User user = (User) ctx.get(USER_INFO_KEY);
-        return user.getActions().stream()
+        if (user == null) {
+            ExceptionUtils.raiseCustomException(HttpStatus.UNAUTHORIZED, "User information not found. Can't execute RBAC filter");
+        }
+        List<Action> actions = user.getActions();
+        return actions.stream()
                 .anyMatch(action -> isActionMatchingIncomingURI(requestUri, action));
     }
 
@@ -75,6 +81,15 @@ public class RbacFilter extends ZuulFilter{
         ctx.set(ERROR_CODE_KEY, status.value());
         ctx.set(ERROR_MESSAGE_KEY, message);
         ctx.set("error.exception", new RbacException("Role does not have access to this URL"));
+        ctx.setSendZuulResponse(false);
+    }
+    
+    private void abortWithStatus(int status, String message, Exception ex) {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        ctx.set(ERROR_CODE_KEY, status);
+        ctx.set(ERROR_MESSAGE_KEY, message);
+        ctx.set("error.exception", ex);
+        ctx.set(RBAC_BOOLEAN_FLAG_NAME, Boolean.FALSE); 
         ctx.setSendZuulResponse(false);
     }
 }
