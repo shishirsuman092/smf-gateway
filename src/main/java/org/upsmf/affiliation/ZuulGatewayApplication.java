4f9529eacd67e674abd81a336f9e28c974995fe3
Shishir Suman <shishir.suman@tarento.com>
2023-05-31 09:42:45 +0530

    remove extra code for zuul geteway

---
+++ src/main/java/org/upsmf/affiliation/ZuulGatewayApplication.java
@@ -0,0 +1,32 @@
+package org.upsmf.affiliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulGatewayApplication{
+	
+	 
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+    
+   
+}
